c8b33d8d273adfbdc6d9ac2b47b5c71182040b82
Danny <lunde.andrews@gmail.com>
2022-07-29 14:04:14 -0400

    Make change

--- public/app.js
+++ public/app.js
@@ -1,9 +1,5 @@
-// fetch("/games")
-//   .then((res) => res.json())
-//   .then((games) => {
-//     document.write(JSON.stringify(games));
-//   });
-
-// $.get("/games", (res) => {
-//   console.log(res);
-// });
+fetch("/games")
+  .then((res) => res.json())
+  .then((games) => {
+    document.write(JSON.stringify(games));
+  });
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 import express from "express";
 import pg from "pg";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const pool = new pg.Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 const PORT = process.env.PORT;
